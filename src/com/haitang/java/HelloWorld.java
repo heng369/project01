e6223af92e6d91bb26f2d81b9bd2f9ae04af5a5e
Heng <1161440598@qq.com>
2020-06-09 20:50:02 +0800

    Initial commit

---
+++ src/com/haitang/java/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.haitang.java;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class HelloWorld {
+    public static void main(String[] args){
+        System.out.println("helloWorld!!");
+        ArrayList list1 = new ArrayList();
+        Date date = new Date();
+        list1.add(0,123);
+    }
+    public void method(){
+        System.out.println();
+    }
+
+}
---
+++ src/com/haitang/java/hello.java
@@ -0,0 +1,11 @@
+package com.haitang.java;
+
+/**
+ * @date 2020/6/8 - 12:53
+ */
+public class hello {
+
+    public static void main(String[] args) {
+
+    }
+}
